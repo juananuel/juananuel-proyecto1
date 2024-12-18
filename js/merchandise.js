ccf004dd1b15ad531e4ebd5f337d1c57abd943bf
Juan Anuel <juancarlosanuel@gmail.com>
2024-12-18 18:23:12 -0300

    update w/js shop

--- js/merchandise.js
+++ js/merchandise.js
@@ -1,19 +1,7 @@
-import { merchandise } from "./data.js";
 
 const cardsMerchandise = document.getElementById('merchandise-cards')
 let productos = [];
 let total = 0;
-
-merchandise.forEach(item => {
-  const card = document.createElement('div')
-  card.classList.add('merchandise-card')
-  card.innerHTML = `
-  <img class="card-img" src="${item.url}" alt="${item.title}">
-  <p class="text-light">Price: $ ${item.price}</p>
-  <button id="btnComprar" class="btn btn-light btn-comprar" onclick="agregarProducto(${item.type}, ${item.price})">Buy</button>
-  `
-  cardsMerchandise.appendChild(card)
-})
 
 function agregarProducto(producto, precio) {
   let carrito = document.getElementById("carrito");
@@ -35,7 +23,7 @@ function pagar() {
   localStorage.setItem("total", total);
 
   alert("Total a pagar: $ " + total);
-  window.location.href = "compra.html"; // Redirige a la página de compra.html
+  window.location.href = "../pages/purchase.html"; // Redirige a la página de compra.html
 }
 
 function limpiarCarrito() {
